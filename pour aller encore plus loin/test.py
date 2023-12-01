1bf05167297389fb60e3878540a94a7b8830e18e
saidali-tsaravelonaallaoui <said-ali.tsaravelona-allaoui@laplateforme.io>
2023-12-01 08:40:57 +0100

    pour aller encore plus loin (Appuyer 'p' pour mettre en pause/reprendre)

---
+++ pour aller encore plus loin/test.py
@@ -0,0 +1,80 @@
+import time
+import keyboard
+
+temps_tuple = (12, 59, 40)
+temps_liste = list(temps_tuple)
+
+def demander_alarme():
+    reponse = input("Voulez-vous définir une alarme? (o/n): ").lower()
+    if reponse == "o":
+        heures = int(input("Heures: "))
+        minutes = int(input("Minutes: "))
+        secondes = int(input("Secondes: "))
+        return heures, minutes, secondes
+    else:
+        return None
+
+def format_heure(param, mode_12h=False):
+    heures, minutes, secondes = param
+    if mode_12h:
+        suffixe = "AM" if heures < 12 else "PM"
+        heures = heures % 12 or 12
+        return f"{heures:02d}:{minutes:02d}:{secondes:02d} {suffixe}"
+    else:
+        return f"{heures:02d}:{minutes:02d}:{secondes:02d}"
+
+def afficher_heure(param, mode_12h=False):
+    try:
+        alarme = demander_alarme()
+        pause = False
+        while True:
+            if keyboard.is_pressed('p'):
+                pause = not pause
+                time.sleep(1)  
+            if not pause:
+                if alarme:
+                    heures, minutes, secondes = alarme
+                    if param[0] == heures and param[1] == minutes and param[2] == secondes:
+                        print("\nAlarme atteinte!")
+                    else:
+                        heure_formattee = format_heure(param, mode_12h)
+                        print(f"Heure actuelle : {heure_formattee}", end='\r')
+                else:
+                    heure_formattee = format_heure(param, mode_12h)
+                    print(f"Heure actuelle : {heure_formattee}", end='\r')
+
+                param[2] += 1
+                if param[2] == 60:
+                    param[2] = 0
+                    param[1] += 1
+                    if param[1] == 60:
+                        param[1] = 0
+                        param[0] += 1
+                        if param[0] == 24:
+                            param[0] = 0
+
+                time.sleep(1)
+    except KeyboardInterrupt:
+        print('\nArrêt du programme')
+        import sys
+        sys.exit(0)
+
+def new_hour(hour_param): 
+    regler_heure = input("Voulez-vous régler l'heure ? (o/n): ").lower()
+    if regler_heure == "o":
+        nouvelles_heures = int(input("Heures : "))
+        nouvelles_minutes = int(input("Minutes : "))
+        nouvelles_secondes = int(input("Secondes :"))
+        
+        hour_param[0] = nouvelles_heures
+        hour_param[1] = nouvelles_minutes
+        hour_param[2] = nouvelles_secondes
+        
+        print("Temps mis à jour :", hour_param)
+    else:
+        print("L'heure n'a pas été modifiée.")
+
+
+mode_12h = input("Choisissez le mode d'affichage (12/24): ").lower() == "12"
+new_hour(temps_liste)
+afficher_heure(temps_liste, mode_12h)
